61e48ef60dc95a779185904ddf061f653f6b0348
Aashish Dhawan <aashishdhawan@gmail.com>
2026-03-22 12:47:07 +0530

    host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,7 @@ Rails.application.configure do
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
     "recruit.quicklabs.in",     # Allow requests from example.com
+    "quick-recruit-copy.onrender.com"
   ]
   # Skip DNS rebinding protection for the default health check endpoint.
   config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
