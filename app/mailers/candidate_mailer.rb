9a36b61f6e9caf9c2fbd1b2768257300bc30148c
Aashish Dhawan <aashishdhawan@gmail.com>
2026-04-11 15:41:55 +0530

    Remove some mentions of crownstack

--- app/mailers/candidate_mailer.rb
+++ app/mailers/candidate_mailer.rb
@@ -2,54 +2,54 @@ class CandidateMailer < ApplicationMailer
   def rejection_email
     @candidate = params[:candidate]
     @owner = params[:candidate].owner
-    mail(to: [@candidate.email, @owner.email], subject: "Hiring@Crownstack: Interview Result", reply_to: @owner.email)
+    mail(to: [@candidate.email, @owner.email], subject: "Interview Result", reply_to: @owner.email)
   end
 
   def about_us_email
     @candidate = params[:candidate]
     @owner = params[:candidate].owner
-    mail(to: [@candidate.email, @owner.email], subject: "Hiring@Crownstack: Company Profile", reply_to: @owner.email)
+    mail(to: [@candidate.email, @owner.email], subject: "Company Profile", reply_to: @owner.email)
   end
 
   def about_us_and_jd_email
     @candidate = params[:candidate]
     @owner = params[:candidate].owner
-    mail(to: [@candidate.email, @owner.email], subject: "Hiring@Crownstack: About Crownstack & Job Description", reply_to: @owner.email)
+    mail(to: [@candidate.email, @owner.email], subject: "About Us & Job Description", reply_to: @owner.email)
   end
 
   def job_description_email
     @candidate = params[:candidate]
     @owner = params[:candidate].owner
-    mail(to: [@candidate.email, @owner.email], subject: "Hiring@Crownstack: Job Description", reply_to: @owner.email)
+    mail(to: [@candidate.email, @owner.email], subject: "Job Description", reply_to: @owner.email)
   end
 
   def not_a_match_email
     @candidate = params[:candidate]
     @owner = params[:candidate].owner
-    mail(to: [@candidate.email, @owner.email], subject: "Hiring@Crownstack: Update on your application", reply_to: @owner.email)
+    mail(to: [@candidate.email, @owner.email], subject: "Update on your application", reply_to: @owner.email)
   end
 
   def lead_email
     @content = params[:content]
     @candidate = params[:candidate]
-    mail(to: [@candidate.owner.email, User.aashish.email], subject: "Hiring@Crownstack: New Lead from Website")
+    mail(to: [@candidate.owner.email, User.aashish.email], subject: "New Lead from Website")
   end
 
   def f2f_detail_email
     @candidate = params[:candidate]
     @owner = @candidate.owner
-    mail(to: [@candidate.email, @owner.email], bcc: [User.aashish.email], subject: "Hiring@Crownstack: Face-to-Face Interview Details", reply_to: @owner.email)
+    mail(to: [@candidate.email, @owner.email], bcc: [User.aashish.email], subject: "Face-to-Face Interview Details", reply_to: @owner.email)
   end
 
   def position_closed_email
     @candidate = params[:candidate]
     @owner = @candidate.owner
-    mail(to: [@candidate.email, @owner.email], subject: "Hiring@Crownstack: Information on Application", reply_to: @owner.email)
+    mail(to: [@candidate.email, @owner.email], subject: "Information on Application", reply_to: @owner.email)
   end
 
   def send_resume_email
     @candidate = params[:candidate]
     @owner = @candidate.owner
-    mail(to: [@candidate.email, @owner.email], subject: "Hiring@Crownstack: Resume Required", reply_to: @owner.email)
+    mail(to: [@candidate.email, @owner.email], subject: "Resume Required", reply_to: @owner.email)
   end
 end
